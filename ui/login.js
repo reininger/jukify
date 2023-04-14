005ac5f7e2c90ed4d51eccaf84ae0ca5082f14a8
Reid Reininger <reidreininger@outlook.com>
2023-04-14 00:20:56 -0700

    Get logged in user display name

--- ui/login.js
+++ ui/login.js
@@ -2,23 +2,38 @@ const isExpired = expiration => {
 	return Date.now() < expiration;
 };
 
-onload = () => {
+onload = async () => {
 	// check sessionStorage for valid access token
-	let accessToken = sessionStorage.getItem("accessToken");
-	let accessTokenExpiration = sessionStorage.getItem("accessTokenExpiration");
+	let accessToken = JSON.parse(sessionStorage.getItem("accessToken"));
+	let accessTokenExpiration = JSON.parse(sessionStorage.getItem("accessTokenExpiration"));
 
 	// check url parameters for valid access token
-	if (!accessToken || isExpired(accessTokenExpiration)) {
+	if (accessToken === null || isExpired(accessTokenExpiration)) {
 		const urlParams = new URLSearchParams(window.location.hash.substr(1));
-		sessionStorage.setItem("accessToken", urlParams.get("accesstoken"));
-		sessionStorage.setItem("accessTokenExpiration", urlParams.get("expiration"));
+		accessToken = urlParams.get("access_token");
+		accessTokenExpiration = urlParams.get("expires_in");
+		sessionStorage.setItem("accessToken", JSON.stringify(accessToken));
+		sessionStorage.setItem("accessTokenExpiration",
+			JSON.stringify(accessTokenExpiration));
 	}
 
 	// if no valid access token, return
-	if (!accessToken || isExpired(accessTokenExpiration)) {
+	if (accessToken === null || isExpired(accessTokenExpiration)) {
 		return;
 	}
 
+	// retrieve user info
+	const url = "https://api.spotify.com/v1/me";
+	const response = await fetch(url, {
+		method: "GET",
+		headers: {
+			"Authorization": `Bearer ${accessToken}`
+		}
+	});
+	const json = await response.json();
+	const { display_name } = json;
+
+	// display user info
 	const loginButtons = document.getElementsByClassName("login-btn");
-	loginButtons[0].innerText = "Welcome!"
+	loginButtons[0].innerText = `Welcome ${display_name}!`
 }
