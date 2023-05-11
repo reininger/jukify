093bbf90e04ce01010cdcdd8a170774fbe468750
Reid Reininger <reidreininger@outlook.com>
2023-05-10 22:05:09 -0700

    Use LoginManager to handle authentication

--- ui/login.js
+++ ui/login.js
@@ -1,49 +1,24 @@
-const isExpired = expiration => {
-	return Date.now() >= expiration;
-};
+import LoginManager from "./LoginManager.js";
 
 const pause = async () => {
 	let accessToken = JSON.parse(sessionStorage.getItem("accessToken"));
   const response = await fetch("https://api.spotify.com/v1/me/player/pause", {
 		method: "PUT",
 		headers: {
-			"Authorization": `Bearer ${accessToken}`
+			"Authorization": `Bearer ${window.loginManager.user.accessToken}`
 		}
 	});
 }
 
 onload = async () => {
-	// check sessionStorage for valid access token
-	let accessToken = JSON.parse(sessionStorage.getItem("accessToken"));
-	let accessTokenExpiration = JSON.parse(sessionStorage.getItem("accessTokenExpiration"));
-
-	// check url parameters for valid access token
-	if (accessToken === null || isExpired(accessTokenExpiration)) {
-		const urlParams = new URLSearchParams(window.location.hash.substr(1));
-		accessToken = urlParams.get("access_token");
-		accessTokenExpiration = urlParams.get("expires_in") * 1000 + Date.now();
-		sessionStorage.setItem("accessToken", JSON.stringify(accessToken));
-		sessionStorage.setItem("accessTokenExpiration",
-			JSON.stringify(accessTokenExpiration));
-	}
-
-	// if no valid access token, return
-	if (accessToken === null || isExpired(accessTokenExpiration)) {
-		return;
-	}
-
-	// retrieve user info
-	const url = "https://api.spotify.com/v1/me";
-	const response = await fetch(url, {
-		method: "GET",
-		headers: {
-			"Authorization": `Bearer ${accessToken}`
-		}
-	});
-	const json = await response.json();
-	const { display_name } = json;
+	const loginManager = new LoginManager();
+	await loginManager.login()
+	window.loginManager = loginManager
 
 	// display user info
 	const loginButtons = document.getElementsByClassName("login-btn");
-	loginButtons[0].innerText = `Welcome ${display_name}!`
+	loginButtons[0].innerText = `Welcome ${loginManager.user.userName}!`
+
+	const pauseButtons = document.getElementsByClassName("pause-btn");
+	pauseButtons[0].onclick = () => pause();
 }
