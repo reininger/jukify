e28990b4310ef6a1a8a96382a9a2e69e7874ee6f
Reid Reininger <reidreininger@outlook.com>
2023-06-05 19:37:57 -0700

    Test returns false on 401 unauthorized

--- jukify-test/SpotifyImplicitGrantLoginManager.test.js
+++ jukify-test/SpotifyImplicitGrantLoginManager.test.js
@@ -1,11 +1,16 @@
 import SpotifyImplicitGrantSigninManager from "../jukify/js/SpotifyImplicitGrantLoginManager";
 
-test('', () => {
+test('Returns false on 401 unauthorized', async () => {
 	const signinManager = new SpotifyImplicitGrantSigninManager();
+    signinManager.GetSpotifyUserProfile = () => {
+        unauthorizedResponse = new Response(options = {
+            status: 401,
+            statusText: Unauthorized
+        });
+    };
+    signinManager.accessToken = 'someRandomAccessToken1';
 
-	const expiredDateTime = 0;
-	const result = loginManager.isExpired(expiredDateTime);
-
-	expect(result).toBe(true);
+    const result = await signinManager.login();
+	expect(result).toBe(false);
 });
 
