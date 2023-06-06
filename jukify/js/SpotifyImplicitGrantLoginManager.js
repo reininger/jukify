d019aa683dc018dc4c2cdd48e5d29ed7c217886f
Reid Reininger <reidreininger@outlook.com>
2023-06-05 19:37:32 -0700

    Reduce SpotifyImplicitGrantLoginManager interface

--- jukify/js/SpotifyImplicitGrantLoginManager.js
+++ jukify/js/SpotifyImplicitGrantLoginManager.js
@@ -4,64 +4,27 @@
  */
 
 export default class SpotifyImplicitGrantSigninManager {
-    #user;
-    #accessToken;
-    #accessTokenExpirationTime;
-
-    /**
-     * Gets or sets the signed in user.
-     */
-    get user() {
-
-    }
-
-    /**
-     * Gets the access token for the signed in user.
-     */
-    get accessToken() {
-
-    }
-
-    /**
-     * Sets the access token for the signed in user.
-     */
-    set accessToken(accessToken) {
-
-    }
-
-    /**
-     * Gets the access token expiration time.
-     */
-    get accessTokenExpirationTime() {
-
-    }
-
-    /**
-     * Sets the access token expiration time.
-     */
-    set accessTokenExpirationTime(accessToken) {
-
-    }
+    user = null;
+    accessToken = null;
+    accessTokenExpirationTime = null;
 
     /**
      * Begins login flow.
      */
     async login() {
-        // get url params
-        // get user info with access token
-        // 
+        // get user info from spotify
+        // handle errors
+        // set signedin user
     }
 
     /**
      * Begins logout flow.
      */
     async logout() {
-
+        
     }
 
-    getUrlSearchParams() {
-        const urlParams = new URLSearchParams(window.location.hash.substr(1));
-        accessToken = urlParams.get("access_token");
-        accessTokenExpiration = urlParams.get("expires_in") * 1000 + Date.now();
+    async GetSpotifyUserProfile() {
+
     }
 }
