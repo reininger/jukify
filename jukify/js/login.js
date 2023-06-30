a270a12e57f07c88d7b2ea9602c0dc07aecbe739
Reid Reininger <reidreininger@outlook.com>
2023-06-30 05:18:53 -0700

    Add create scripts directory and add "open"

--- jukify/js/login.js
+++ jukify/js/login.js
@@ -5,11 +5,19 @@ onload = async () => {
 	window.loginManager = new LoginManager();
 	window.player = new Player(window.loginManager);
 
+	// configure login button link
+	const clientId = '71b02df30e1d481c807dcd000c603440';
+	const protocol = window.location.protocol;
+	const hostname = window.location.hostname;
+	const port = window.location.port;
+	const redirectUri = `${protocol}//${hostname}:${port}`;
+
 	await loginManager.login()
 
 	// display user info
 	const loginButtons = document.getElementsByClassName("login-btn");
 	loginButtons[0].innerText = `Welcome ${loginManager.user.userName}!`
+	loginButtons.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&scope=user-modify-playback-state%20user-read-private%20user-read-email%20user-read-playback-state`
 
 	const pauseButtons = document.getElementsByClassName("pause-btn");
 	pauseButtons[0].onclick = async () => {
