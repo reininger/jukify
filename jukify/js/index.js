e91a41b92dde7ec056731081480bc77d47918789
Reid Reininger <reidreininger@outlook.com>
2023-08-11 00:23:36 -0700

    Add player js to index

--- jukify/js/index.js
+++ jukify/js/index.js
@@ -2,22 +2,48 @@
  * JS for index page.
  */
 
+import Player from "./Player.js";
 import LoginManager from "./SpotifyImplicitGrantLoginManager.js"
 
 window.onload = async () => {
-    const loginManager = new LoginManager();
+    window.loginManager = new LoginManager();
+    window.player = new Player(window.loginManager);
+    
+    const pauseButton = document.getElementById("pause");
+    const playButton = document.getElementById("play");
+    const nameNode = document.querySelector("header h4");
+
     let result;
     try {
-        result = await loginManager.login();
+        result = await window.loginManager.login();
     } catch {
         // do nothing
     }
-    
-    if (result !== true || !loginManager.user) {
+
+    if (result !== true || !window.loginManager.user) {
         // redirect to login page
         window.location = `html/login.html`
     }
+    nameNode.innerHTML = window.loginManager.user.display_name;
 
-    const nameNode = document.querySelector("header h4");
-    nameNode.innerHTML = loginManager.user.display_name;
-}
+    const setPlayPauseButton = async () => {
+        const playing = await window.player.isPlaying();
+        playButton.style.display = playing ? "none" : "inline";
+        pauseButton.style.display = playing ? "inline" : "none";
+    }
+
+    pauseButton.onclick = async () => {
+        await window.player.pause();
+        setPlayPauseButton();
+    }
+
+    playButton.onclick = async () => {
+        await window.player.play();
+        setPlayPauseButton();
+    }
+
+    setPlayPauseButton();
+
+    //const nextButtons = document.getElementsByClassName("next-btn");
+    //nextButtons[0].onclick = () => window.player.next()
+};
