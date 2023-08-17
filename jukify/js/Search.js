86c8c015ac173a051f662084a5d82ca20ea6025b
Reid Reininger <reidreininger@outlook.com>
2023-08-16 19:49:59 -0700

    Add Search.js

---
+++ jukify/js/Search.js
@@ -0,0 +1,20 @@
+/**
+* Controls the search.
+*/
+
+export default class Search {
+
+	constructor(client) {
+		this._client = client;
+	}
+
+	async search(query, type) {
+		const url = `search?q=${encodeURIComponent(query)}&type=${type}`;
+		const response = await this._client.fetch(url);
+		if (!response.ok) {
+			return {};
+		}
+		return await response.json();
+	}
+}
+
