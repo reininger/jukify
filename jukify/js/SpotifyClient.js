37a3782f1c071d67a9af849b581adeee9dcb8bda
Reid Reininger <reidreininger@outlook.com>
2023-08-16 17:23:34 -0700

    Add baseUrl to SpotifyHttpClient constructor

--- jukify/js/SpotifyClient.js
+++ jukify/js/SpotifyClient.js
@@ -2,10 +2,11 @@
 * Client for interacting with the Spotify http API.
 */
 
+const defaultBaseUrl = "https://api.spotify.com/v1";
 
 class SpotifyHttpClient {
 
-	constructor(fetch, loginManager) {
+	constructor(fetch, loginManager, baseUrl = defaultBaseUrl) {
 		this._fetch = fetch;
 		this._loginManager = loginManager;
 
