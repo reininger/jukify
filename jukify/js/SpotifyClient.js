1d4799e4698cef1ebc24a2ea13443b979d0218b0
Reid Reininger <reidreininger@outlook.com>
2023-08-16 18:13:45 -0700

    Implement SpotifyClient.js

--- jukify/js/SpotifyClient.js
+++ jukify/js/SpotifyClient.js
@@ -2,15 +2,23 @@
 * Client for interacting with the Spotify http API.
 */
 
-const defaultBaseUrl = "https://api.spotify.com/v1";
+const defaultBaseUrl = "https://api.spotify.com/v1/";
 
-export default class SpotifyHttpClient {
+export default class SpotifyClient {
 
 	constructor(fetch, loginManager, baseUrl = defaultBaseUrl) {
 		this._fetch = fetch;
 		this._loginManager = loginManager;
+		this._baseUrl = baseUrl;
+	}
 
-		this._defaultHeaders = {};
+	fetch(path, options) {
+		const url = this._baseUrl + path;
+		options.headers = {
+			...options.headers,
+			Authorization: "Bearer " + this._loginManager.accessToken
+		}
+		return window.fetch(url, options);
 	}
 	
 }
