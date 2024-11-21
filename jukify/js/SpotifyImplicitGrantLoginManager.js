7cf9ba6edcddfe0e31705425a549114c6f190616
Reid Reininger <reidreininger@outlook.com>
2024-11-20 14:11:33 -1000

    Fix whitespace

--- jukify/js/SpotifyImplicitGrantLoginManager.js
+++ jukify/js/SpotifyImplicitGrantLoginManager.js
@@ -2,19 +2,18 @@
  * Responsible for handling the logged in user using Spotify's implicit grant
  * flow.
  */
-
 export default class SpotifyImplicitGrantSigninManager {
   user = null
   accessToken = null
   accessTokenExpirationTime = null
 
   /**
-     * Begins login flow.
-     *
-     * Returns true on successful login. Returns false on failed login due to
-     * bad access token. Throws on server errors.
-     */
-  async login () {
+   * Begins login flow.
+   *
+   * Returns true on successful login. Returns false on failed login due to
+   * bad access token. Throws on server errors.
+   */
+  async login() {
     if (this.user) {
       this.user = null
       throw new Error('Cannot call login when a user is already logged in')
@@ -46,29 +45,29 @@ export default class SpotifyImplicitGrantSigninManager {
   }
 
   /**
-     * Begins logout flow.
-     */
-  async logout () {
+  * Begins logout flow.
+  */
+  async logout() {
     this.user = null
     this.accessToken = null
     this.accessTokenExpirationTime = null
   }
 
   /**
-     * Sets access token and expiration time.
-     */
-  UpdateAuthenticationArguments () {
+  * Sets access token and expiration time.
+  */
+  UpdateAuthenticationArguments() {
     const urlParams = new URLSearchParams(window.location.hash.substr(1))
     this.accessToken = urlParams.get('access_token')
     this.accessTokenExpirationTime = urlParams.get('expires_in') * 1000 + Date.now()
   }
 
   /**
-     * Calls Spotify API to get user profile.
-     *
-     * Returns a Response object.
-     */
-  async GetSpotifyUserProfile () {
+  * Calls Spotify API to get user profile.
+  *
+  * Returns a Response object.
+  */
+  async GetSpotifyUserProfile() {
     const url = 'https://api.spotify.com/v1/me'
     const response = await fetch(url, {
       method: 'GET',
@@ -79,7 +78,7 @@ export default class SpotifyImplicitGrantSigninManager {
     return response
   }
 
-  IsExpired () {
+  IsExpired() {
 
   }
 }
