c77298bde18ff098061cd6e4950b95b5792eb5da
Reid Reininger <reidreininger@outlook.com>
2024-11-23 21:53:59 -1000

    Refactor Track

--- jukify/components/Track.js
+++ jukify/components/Track.js
@@ -2,40 +2,46 @@ export default class Track extends HTMLElement {
     static type = 'spotify-track'
 
     connectedCallback() {
-        this.init()
-    }
-
-    init() {
         this.classList.add(
             "p-0", "list-group-item", "d-flex", "align-items-end", "border",
             "rounded-end"
         )
 
-        this.id = this.dataset.id
+        this.replaceChildren(
+            this.trackImageFactory(),
+            this.headingFactory(),
+            this.removeButtonFactory()
+        )
+    }
 
-        const trackImage = document.createElement('img')
-        trackImage.height = 64;
-        trackImage.width = 64;
-        trackImage.classList.add('me-2')
-        trackImage.src = this.dataset.image
-        this.appendChild(trackImage)
-
+    headingFactory() {
         const heading = document.createElement('h6')
         heading.classList.add("m-0", "flex-grow-1")
         heading.textContent = `${this.dataset.name} by ${this.dataset.artist} - ${this.dataset.album}`
-        this.appendChild(heading)
+        return heading
+    }
 
+    removeButtonFactory() {
         const removeButton = document.createElement('button')
         removeButton.textContent = '-'
         removeButton.classList.add(
             "btn", "btn-danger", "align-self-stretch", "rounded-0",
             "rounded-end"
         )
-        
+
         removeButton.addEventListener('click', (e) => {
             this.remove()
         })
 
-        this.appendChild(removeButton)
+        return removeButton
+    }
+
+    trackImageFactory() {
+        const trackImage = document.createElement('img')
+        trackImage.height = 64;
+        trackImage.width = 64;
+        trackImage.classList.add('me-2')
+        trackImage.src = this.dataset.image
+        return trackImage
     }
 }
